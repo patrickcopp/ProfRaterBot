6591de5c151b9049ebb025844fc2f6b80f7787d5
Patrick Copp <pcopp@uoguelph.ca>
2021-05-28 21:19:53 -0400

    Adding validator

---
+++ codes.py
@@ -0,0 +1,5 @@
+class ResponseCodes:
+    OK = 0
+    WRONG_NUMBER_ARGS = 1
+    ARGS_MESSED_UP = 2
+    NUMBERS_MESSED_UP = 3
---
+++ validator.py
@@ -0,0 +1,36 @@
+from codes import ResponseCodes
+
+def add_prof_val(content: str):
+    if len(content.split()) != 3:
+        return ResponseCodes.WRONG_NUMBER_ARGS
+    return ResponseCodes.OK
+
+def remove_prof_val(content: str):
+    if len(content.split()) != 3:
+        return ResponseCodes.WRONG_NUMBER_ARGS
+    return ResponseCodes.OK
+
+def remove_rating_val(content: str):
+    if len(content.split()) != 3:
+        return ResponseCodes.WRONG_NUMBER_ARGS
+    if content.split()[1][0] != '<' or content.split()[1][-1] != '>':
+        return ResponseCodes.ARGS_MESSED_UP
+
+def rate_val(content: str):
+    if len(content.split()) != 5:
+        return ResponseCodes.WRONG_NUMBER_ARGS
+    try:
+        if int(content.split()[2]) > 10 or int(content.split()[2]) < -1:
+            return ResponseCodes.NUMBERS_MESSED_UP
+        if int(content.split()[3]) > 10 or int(content.split()[2]) < -1:
+            return ResponseCodes.NUMBERS_MESSED_UP
+        if int(content.split()[4]) > 10 or int(content.split()[2]) < -1:
+            return ResponseCodes.NUMBERS_MESSED_UP
+    except:
+        return ResponseCodes.ARGS_MESSED_UP
+    return ResponseCodes.OK
+
+def remove_val(content: str):
+    if len(content.split()) != 3:
+        return ResponseCodes.WRONG_NUMBER_ARGS
+    return ResponseCodes.OK
